c926040498f879a8f5367f89adc408ac1a231f2a
Søren Magnusson <smag@tec.dk>
2020-07-30 05:27:31 +0200

    Har nu flere tråde og en "portlist" som nærmest er en global tæller, så den kan afprøve flere porte samtididgt. Det går _MEGET_ hurtigere.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿using System;
+﻿/*
+ * Stærk inspireret af Munir Usman http://munir.wordpress.com
+ *                     Contact: munirus@gmail.com 
+ *                     https://github.com/munirusman/Simple-TCP-Port-Scanner.git
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PortScanner
@@ -21,9 +29,37 @@ namespace PortScanner
 
             int startPort = 1;
             int endPort = 65535;
+            PortList portlist = new PortList(startPort, endPort);
             TcpClient tcp = new TcpClient();
 
-            for (int port = startPort; port < endPort; port++)
+            int numThreads = 200;
+            for (int i = 0; i < numThreads; i++)
+            {
+                PortScanner scanner = new PortScanner(ipAdrrToScan, portlist);
+                Thread th = new Thread(new ThreadStart(scanner.run));
+                th.Start();
+            }
+
+        }
+    }
+
+    class PortScanner
+    {
+        private int port;
+        private PortList portlist;
+        IPAddress ipAdrrToScan;
+
+        public PortScanner(IPAddress ipAdrrToScan, PortList portList)
+        {
+            this.portlist = portList;
+            this.ipAdrrToScan = ipAdrrToScan;
+        }
+
+        public void run()
+        {
+            int port;
+            TcpClient tcp = new TcpClient();
+            while ((port = portlist.getNext()) != -1)
             {
                 tcp = new TcpClient();
                 try
@@ -42,6 +78,36 @@ namespace PortScanner
                     tcp.Close();
                 }
             }
+        }
+    }
+
+    public class PortList
+    {
+        private int start;
+        private int stop;
+        private int ptr;
+
+        public PortList(int start, int stop)
+        {
+            this.start = start;
+            this.stop = stop;
+            this.ptr = start;
+        }
+
+        public PortList() : this(1, 65535)
+        {
+        }
+
+        public bool hasMore()
+        {
+            return (stop - ptr) >= 0;
+        }
+
+        public int getNext()
+        {
+            if (hasMore())
+                return ptr++;
+            return -1;
         }
     }
 }
